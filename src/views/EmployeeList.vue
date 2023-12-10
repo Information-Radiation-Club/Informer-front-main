ecfed1e9daa1384332d84bd666c1425344341de4
Atefeh Hemmati <68479127+Atefeh97hmt@users.noreply.github.com>
2023-12-10 23:25:51 +0330

    Add files via upload

---
+++ src/views/EmployeeList.vue
@@ -0,0 +1,17 @@
+
+<template>
+
+    <div class="font-sans bg-secondary">
+      <Sidebar />
+      <MainContent />
+    </div>
+  </template>
+  
+  <script setup>
+  import Sidebar from "@/components/Sidebar.vue";
+  import MainContent from "@/components/MainContent.vue";
+  </script>
+  
+
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <main class="font-sans bg-secondary">
+
+    <router-link to="/employees">  employee list </router-link>
+
+  </main>
+</template>
+
+<style>
+</style>
